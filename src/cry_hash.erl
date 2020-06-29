0d0dee9f8657a05a42dfce03ffe62c1421d47b70
whn <whn@qq.com>
2020-06-29 20:50:35 +0800

    s

--- src/cry_hash.erl
+++ src/cry_hash.erl
@@ -6,7 +6,7 @@
 %% API functions
 %% ====================================================================
 
--export([md5/1, sha/1, sha256/1, hmacsha256/2, sha512/1, hmacsha512/2, des_encode/2]).
+-export([md5/1, sha/1, sha256/1, hmacsha256/2, sha512/1, hmacsha512/2, des_encode/2, des_decode/2]).
 
 md5(List) ->
   Bin = list_to_binary([io_lib:format("~2.16.0b", [N]) || N <- binary_to_list(erlang:md5(List))]),
@@ -35,8 +35,14 @@ hmacsha512(List, Key) ->
 %% Data=string/binary
 des_encode(Data, Key) ->
     ParS = des_plain_text(Data),
-    Des = crypto:block_encrypt(des_cbc, Key, Key, ParS),
+    Des = crypto:block_encrypt(des_ecb, Key, ParS),
     base64:encode(Des).
+
+des_decode(Key, Bin) ->
+    Binary = crypto:block_decrypt(des_ecb, Key, base64:decode(Bin)),
+    List = binary_to_list(Binary),
+    L = [Num | _] = lists:reverse(List),
+    do_clean_padding(Num, Num, L).
 
 %% ====================================================================
 %% Internal functions
@@ -49,6 +55,12 @@ des_plain_text(Data) ->
     N = 8 - (erlang:byte_size(Data) rem 8),
     NewData = erlang:binary_to_list(Data),
     lists:append(NewData, get_padding(N)).
+
+
+do_clean_padding(0, _, List) -> lists:reverse(List);
+do_clean_padding(Num, Val, List) ->
+    [_|T] = List,
+    do_clean_padding(Num-1,Val,T).
 
 %% DES 规则
 get_padding(N) ->
