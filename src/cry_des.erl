bbfc543d8f0e323f1f20b409a80028b4793ab223
whn <whn@qq.com>
2020-07-29 17:37:15 +0800

    modify cry_des.erl make to_2 && to_16 into to_bin && to_hex

--- src/cry_des.erl
+++ src/cry_des.erl
@@ -10,18 +10,18 @@
 -author("whn").
 
 %% API
--export([des_ecb_encode/2, des_cbc_encode/2, des_ecb_decode/2, des_cbc_decode/2, to_2/1, to_16/1, get_padding/1]).
+-export([des_ecb_encode/2, des_cbc_encode/2, des_ecb_decode/2, des_cbc_decode/2, to_hex/1, get_padding/1, to_bin/1]).
 
 
 des_ecb_encode(Key, Data) ->
     ParS1 = des_plain_text(Data),
     Bin = crypto:block_encrypt(des_ecb, Key, ParS1),
     Bin.
-%%    binary_to_list(to_16(Bin)).
+%%    binary_to_list(to_hex(Bin)).
 des_cbc_encode(Key, Data) ->
     ParS1 = des_plain_text(Data),
     Bin = crypto:block_encrypt(des_cbc, Key, <<0:64>>, ParS1),
-    binary_to_list(to_16(Bin)).
+    binary_to_list(to_hex(Bin)).
 
 des_ecb_decode(Key, Bin) ->
 %%    Bin = base64:decode(Data),
@@ -39,20 +39,59 @@ des_cbc_decode(Key, Bin) ->
 
 
 
-to_16(List) ->
-    list_to_binary([io_lib:format("~2.16.0b", [N]) || N <- binary_to_list(List)]).
 
-to_2(List) ->
-    to_2(List, []).
-to_2([], R) ->
-    erlang:list_to_binary(lists:reverse(R));
-to_2(Binary, R) when is_binary(Binary)->
-    to_2(erlang:binary_to_list(Binary), R);
-to_2(List, R) ->
-    A = lists:sublist(List,2),
-    NewList = List--A,
-    NewR = [erlang:list_to_integer(A,16) | R],
-    to_2(NewList, NewR).
+%% @spec to_hex(integer | iolist()) -> string()
+%% @doc Convert an iolist to a hexadecimal string.
+to_hex(0) ->
+    "0";
+to_hex(I) when is_integer(I), I > 0 ->
+    to_hex_int(I, []);
+to_hex(B) ->
+    to_hex(iolist_to_binary(B), []).
+
+%% @spec to_bin(string()) -> binary()
+%% @doc Convert a hexadecimal string to a binary.
+to_bin(L) ->
+    to_bin(L, []).
+
+%% @spec to_int(string()) -> integer()
+%% @doc Convert a hexadecimal string to an integer.
+to_int(L) ->
+    erlang:list_to_integer(L, 16).
+
+%% @spec dehex(char()) -> integer()
+%% @doc Convert a hex digit to its integer value.
+dehex(C) when C >= $0, C =< $9 ->
+    C - $0;
+dehex(C) when C >= $a, C =< $f ->
+    C - $a + 10;
+dehex(C) when C >= $A, C =< $F ->
+    C - $A + 10.
+
+%% @spec hexdigit(integer()) -> char()
+%% @doc Convert an integer less than 16 to a hex digit.
+hexdigit(C) when C >= 0, C =< 9 ->
+    C + $0;
+hexdigit(C) when C =< 15 ->
+    C + $a - 10.
+
+%% Internal API
+
+to_hex(<<>>, Acc) ->
+    lists:reverse(Acc);
+to_hex(<<C1:4, C2:4, Rest/binary>>, Acc) ->
+    to_hex(Rest, [hexdigit(C2), hexdigit(C1) | Acc]).
+
+to_hex_int(0, Acc) ->
+    Acc;
+to_hex_int(I, Acc) ->
+    to_hex_int(I bsr 4, [hexdigit(I band 15) | Acc]).
+
+to_bin([], Acc) ->
+    iolist_to_binary(lists:reverse(Acc));
+to_bin([C1, C2 | Rest], Acc) ->
+    to_bin(Rest, [(dehex(C1) bsl 4) bor dehex(C2) | Acc]).
+
 
 
 
